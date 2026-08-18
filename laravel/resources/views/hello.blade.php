cf758ad6df4f25d27e39f2a79bf3c5c345e85d7d
Kawabata-Yoshiki <yoshiki-kawabata@toko-ss.co.jp>
2026-08-18 15:55:10 +0900

    Update greeting and add deployment information
    Updated the greeting and added deployment details.

--- laravel/resources/views/hello.blade.php
+++ laravel/resources/views/hello.blade.php
@@ -5,6 +5,8 @@
   <title>Hello AWS CodeDeploy</title>
 </head>
 <body>
-  <h2>Hello AWS CodeDeploy!!</h2>
+  <h2>Hello AWS CodeDeploy!! v2</h2>
+  <p>Updated by CI/CD pipeline.</p>
+  <p>Deploy test: 2026-08-18</p>
 </body>
 </html>
